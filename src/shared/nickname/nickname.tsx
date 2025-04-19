d07b9c09f553a229558e3c437fa5f4a90aa202cb
MaxOstapets <maksostapets2007@gmail.com>
2025-04-19 10:55:09 +0300

    fixed reusable components problem

--- src/shared/nickname/nickname.tsx
+++
@@ -1,5 +0,0 @@
-export const Nickname = () => {
-    return (
-        <input type="email" className="w-xs border-4 border-blue-600 text-blue-600 p-2" />
-    )
-}
--- src/shared/password/password.tsx
+++
@@ -1,5 +0,0 @@
-export const Password = () => {
-    return (
-        <input type="password" className="w-xs border-4 border-blue-600 text-blue-600 p-2" />
-    )
-}
